11c2e8a8acec0e59c0a86fda18120969c5540561
Ilma2003819 <as20240482@sci.sjp.ac.lk>
2025-10-21 22:32:19 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CITIES 30
+#define MAX_DELIVERIES 50
+
+void printMainMenu();
+void cityManagement();
+void distanceManagement();
+void addNewCity();
+void removeCity();
+void renameCity();
+void inputDistanceBetweenCities();
+void displayDistanceTable();
+void vehicleManagement();
+void calculations();
+void generateReport();
+
+
+int main()
+{
+
+    return 0;
+}
